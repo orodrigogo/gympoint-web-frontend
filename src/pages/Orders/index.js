7d5a1962563e26dde21b425f4d2591b88ea6bde0
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-11 15:47:06 -0300

    feat: pagination orders

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Content } from "./styles";
+import { Container, Content, PageActions } from "./styles";
 
 import api from "../../services/api";
 import Modal from "../../components/ModalOrder";
@@ -8,20 +8,26 @@ import Modal from "../../components/ModalOrder";
 export default function Orders() {
   const [openModal, setOpenModal] = useState([]);
   const [orders, setOrders] = useState([]);
+  const [page, setPage] = useState(1);
+
+  async function loadOrders() {
+    await api
+      .get(`pendingquestions?page=${page}`)
+      .then(response => setOrders(response.data));
+  }
 
   useEffect(() => {
-    async function loadOrders() {
-      await api
-        .get("pendingquestions")
-        .then(response => setOrders(response.data));
-    }
-
     loadOrders();
-  }, []);
+  }, [page]);
 
   function handleShowModal(payload) {
     setOpenModal({ open: !openModal.open, payload });
   }
+
+  function handlePage(action) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
+
   return (
     <>
       {openModal.open && <Modal payload={openModal.payload} />}
@@ -55,6 +61,19 @@ export default function Orders() {
             </tbody>
           </table>
         </Content>
+        <PageActions>
+          <button
+            type="button"
+            onClick={() => handlePage("back")}
+            disabled={page < 2}
+          >
+            Anterior
+          </button>
+          <span>Página {page}</span>
+          <button type="button" onClick={() => handlePage("next")}>
+            Próximo
+          </button>
+        </PageActions>
       </Container>
     </>
   );
